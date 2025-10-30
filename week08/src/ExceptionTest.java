5d5b192582b193d4a3593d626a3e4831e1e4af2b
rhkswkd1224 <rhkswkd1224@naver.com>
2025-10-30 20:25:27 +0900

    exception handling v0.1

---
+++ week08/src/ExceptionTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input numerator : ");
+        int numerator = scanner.nextInt();
+        System.out.print("Input denominator : ");
+        //double denominator = scanner.nextDouble();
+        int denominator = scanner.nextInt();
+
+        if(denominator == 0){
+            System.out.println("분모에 0이 올 수 없습니다!");
+        }else{
+            System.out.println(numerator/denominator);
+        }
+    }
+}
